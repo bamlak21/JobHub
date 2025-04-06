03a1c499b285c27085a263a0715704ed5ab6ea38
Abdisa Merga <abdisamk@gmail.com>
2025-04-06 20:03:54 +0300

    change: (#minor) increased duration to 72h / results 40

--- frontend/app/api/indeed/route.ts
+++ frontend/app/api/indeed/route.ts
@@ -16,8 +16,8 @@ export async function POST(request: Request) {
         search_term: body.search_term,
         google_search_term: body.google_search_term,
         location: body.location,
-        results_wanted: body.results_wanted || 20,
-        hours_old: body.hours_old || 24,
+        results_wanted: body.results_wanted || 40, // increased the result wanted
+        hours_old: body.hours_old || 72, // the duration to two days
         country_indeed: body.country_indeed || "USA",
       }),
       signal: controller.signal,
